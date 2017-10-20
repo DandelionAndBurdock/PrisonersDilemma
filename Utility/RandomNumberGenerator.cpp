66e0a1169ea0eeae20e1bbb79990043e201b18d0
DandelionAndBurdock <philipjones87@hotmail.com>
2017-10-20 16:06:54 +0100

    Added Random Number Generator Singleton Class

---
+++ Utility/RandomNumberGenerator.cpp
@@ -0,0 +1,47 @@
+// Class: Random Number Generator
+// Description: Handles the generation of random numbers 
+// Author: Philip Jones
+// Date: 17/10/2017
+
+#include "RandomNumberGenerator.h"
+
+#include <stdlib.h>		// For srand(), rand()
+#include <time.h>		
+RandomNumberGenerator::RandomNumberGenerator()
+{
+	// Initialise random seed
+	srand(time(nullptr));
+}
+
+
+RandomNumberGenerator::~RandomNumberGenerator()
+{
+}
+
+
+RandomNumberGenerator* RandomNumberGenerator::Instance()
+{
+	static RandomNumberGenerator instance;
+	return &instance;
+}
+
+
+float RandomNumberGenerator::GetRandFloat(float min, float max){
+	float random = rand() / float(RAND_MAX);		
+	return min + (max - min) * random;
+}
+
+
+int RandomNumberGenerator::GetRandInt(int min, int max){
+	return min + (rand() % static_cast<int>(max - min));
+}
+
+
+
+int RandomNumberGenerator::GetExcludedRandInt(int min, int max, int excluded){
+	int randNo = excluded;
+	while (randNo == excluded){
+		randNo = min + (rand() % static_cast<int>(max - min));
+	}
+	return randNo;
+}
---
+++ Utility/RandomNumberGenerator.h
@@ -0,0 +1,24 @@
+// Class: Interpreter
+// Description: Executes the code of 
+// and input strategy string
+// Author: Philip Jones
+// Date: 17/10/2017
+
+
+#pragma once
+class RandomNumberGenerator
+{
+public:
+	static RandomNumberGenerator* Instance();
+	// TODO: Will this interval be closed or open?
+	// Returns a random float in the closed interval [min, mix] 
+	float GetRandFloat(float min = 0.0f, float max = 1.0f);
+	// Returns a random int in the closed interval [min, mix] 
+	int GetRandInt(int min, int max);
+	// Get a random int in the range [min, max) which is not the excluded int
+	int GetExcludedRandInt(int min, int max, int excluded);
+
+private:
+	RandomNumberGenerator();
+	~RandomNumberGenerator();
+};
